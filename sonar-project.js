5207960aa8676d9eb5813c91d712f6ac14572b94
olagokeone <119080085+olagokeone@users.noreply.github.com>
2023-01-02 19:56:31 -0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -7,7 +7,7 @@ sonarqubeScanner({
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
 	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
+	    'sonar.password': 'admin123',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
